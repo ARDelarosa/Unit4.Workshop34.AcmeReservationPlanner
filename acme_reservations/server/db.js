ff365f46f00ceacf43afcdd789b7030c21b58c9d
ARDelarosa <63564596+ARDelarosa@users.noreply.github.com>
2024-08-14 21:10:57 -0700

    upload inital files

---
+++ acme_reservations/server/db.js
@@ -0,0 +1,101 @@
+const pg = require('pg');
+const client = new pg.Client(
+    process.env.DATABASE_URL || 'postgres://postgres:Viper001@localhost:5432/acme_reservation_planner');
+const uuid = require('uuid');
+
+// this is our server database
+// Parents tables are customers and restaurants; child table is reservations
+const createTables = async ()=>{
+    const SQL =`
+    DROP TABLE IF EXISTS reservations CASCADE;
+    DROP TABLE IF EXISTS customers CASCADE;
+    DROP TABLE IF EXISTS restaurants CASCADE;
+    CREATE TABLE customers(
+    id UUID PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL
+    );
+    CREATE TABLE restaurants(
+    id UUID PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL
+    );
+    CREATE TABLE reservations(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    customer_id UUID REFERENCES customers(id) NOT NULL,
+    restaurant_id UUID REFERENCES restaurants(id) NOT NULL,
+    date DATE NOT NULL,
+    party_count INTEGER NOT NULL
+    );
+    `
+    await client.query(SQL);
+    console.log('tables created');
+};
+
+ const createCustomer = async (customerName)=>{
+    const SQL = `
+    INSERT INTO customers(id, name) 
+    VALUES($1, $2)
+    RETURNING *;
+    `
+    const response = await client.query(SQL, [uuid.v4(), customerName]);
+    return response.rows[0];
+}
+
+const createRestaurant = async (restaurantName)=>{
+    const SQL = `
+    INSERT INTO restaurants(id, name) 
+    VALUES($1, $2)
+    RETURNING *;
+    `
+    const response = await client.query(SQL, [uuid.v4(), restaurantName]);
+    return response.rows[0];
+}
+
+const createReservation = async (customer_id, restaurant_id, date, party_count)=>{
+    const SQL = `
+    INSERT INTO reservations(customer_id, restaurant_id, date, party_count) 
+    VALUES($1, $2, $3, $4)
+    RETURNING *;
+    `
+    const response = await client.query(SQL, [customer_id, restaurant_id, date, party_count]);
+    return response.rows[0];
+}
+
+const destroyReservation = async (id, customer_id)=>{
+    const SQL = `
+    DELETE FROM reservations
+    WHERE id = $1 AND customer_id = $2;
+    `
+    await client.query(SQL, [id, customer_id]);
+    return 'deleted';
+}
+
+
+const fetchCustomers = async ()=>{
+    const SQL = `
+    SELECT *
+    FROM customers;
+    `
+    const response = await client.query(SQL);
+    return response.rows;
+}
+
+const fetchRestaurants = async ()=>{
+    const SQL = `
+    SELECT *
+    FROM restaurants;
+    `
+    const response = await client.query(SQL);
+    return response.rows;
+}
+
+
+module.exports = {
+    client,
+    createTables,
+    createCustomer,
+    createRestaurant,
+    createReservation,
+    destroyReservation,
+    fetchCustomers,
+    fetchRestaurants,
+};
---
+++ acme_reservations/server/index.js
@@ -0,0 +1,92 @@
+const express = require ('express');
+const app = express()
+const {
+    client,
+    createTables,
+    createCustomer,
+    createRestaurant,
+    createReservation,
+    destroyReservation,
+    fetchCustomers,
+    fetchRestaurants,
+    } = require ('./db');
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(require('morgan')('dev'));
+
+//get customers
+app.get('/api/customers', async (req, res, next)=>{
+    try {
+        res.send(await fetchCustomers())
+    } catch (error) {
+        next(error)
+    }
+})
+//get restaurants
+app.get('/api/restaurants', async (req, res, next)=>{
+    try {
+        res.send(await fetchRestaurants())
+    } catch (error) {
+        next(error)
+    }
+})
+//post new reservation
+app.post('/api/customers/:customer_id/reservations', async (req, res, next)=>{
+    try {
+        const customer_id = req.params.customer_id
+        const restaurant_id = req.body.restaurant_id
+        const date = req.body.date
+        res.send(await createReservation(customer_id, restaurant_id, date))
+    } catch (error) {
+        next(error)
+    }
+})
+//delete reservation
+app.delete('/api/customers/:customer_id/reservations', async (req, res, next)=>{
+    try {
+        const customerID = req.params.customer_id
+        const id = req.params.id
+        await destroyReservation(id, customerID) && res.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// This is our server express
+// Where are SQL is created
+const init = async()=>{
+    await client.connect();
+    console.log('connected to the database');
+    await createTables();
+    console.log("Tables have been created");
+
+    await Promise.all([
+        createRestaurant("Sorentos"),
+        createRestaurant("Cowboy Star"),
+        createRestaurant("Steamy Piggy"),
+        createCustomer("David Smith"),
+        createCustomer("Amy Jones"),
+        createCustomer("Ken Wilson")]);
+    const customers = await fetchCustomers()
+    const restaurants = await fetchRestaurants()
+    console.log("customers seeded and they are :", customers, "restaurants seeded and they are :", restaurants);
+
+    await createReservation(
+        customers[2].id,
+        restaurants[0].id,
+        '2024-12-25',
+        4
+    );
+    //const reservations = await fetchReservations();
+
+    //await destroyReservation([0].id, users[2].id);
+    //const newReservations = await fetchReservations();
+    //console.log("and reservations too!:", reservations);
+    console.log("the data was seeded!")
+
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+}
+
+init();
